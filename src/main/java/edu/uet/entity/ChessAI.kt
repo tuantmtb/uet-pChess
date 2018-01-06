b9c6df4b8faa465deff6b348c32be467bdfb328f
Tran Minh Tuan <tuantmtb@gmail.com>
2018-01-06 18:31:13 +0700

    thread for AI

--- src/main/java/edu/uet/entity/ChessAI.kt
+++ src/main/java/edu/uet/entity/ChessAI.kt
@@ -17,51 +17,46 @@ class ChessAI(val color: ChessSide = ChessSide.BLACK) {
      * don't call callback if canceled
      */
     fun getNextMoveForChessBoard(chessBoard: ChessBoard, points: HashMap<ChessSide, Int?>, callback: ((ChessPiece, ChessPiece.Position) -> Unit)) {
-
-//        // create game space
-//        val gameSpaceRoot = GameSpaceNode(chessBoard)
-//        val currentGameSpace = GameSpace(gameSpaceRoot)
         println("getNextMoveForChessBoard")
-        val pieces = chessBoard.pieces
-//        for (piece in pieces) {
-//            val side = if (piece.chessSide == ChessSide.BLACK) "n" else "N"
-//            print("[" + piece.position.x + "][" + piece.position.y + "]=" + side)
-//        }
-//        println()
-        val chessBoardRotated = rotateChessBoard90Left(chessBoard)
-        val bitBoardPair = TranformBitboardAndView().viewModelToAIModel(chessBoardRotated)
+        Thread {
 
-        val array = TranformBitboardAndView().viewModelToArray(chessBoardRotated)
-        for (i in 0..7) {
-            println(Arrays.toString(array[i]))
-        }
+            val chessBoardRotated = rotateChessBoard90Left(chessBoard)
+            val bitBoardPair = TranformBitboardAndView().viewModelToAIModel(chessBoardRotated)
 
-        val ai = AI(Zobrist())
-        val startTime = System.currentTimeMillis()
-        val deepSearch = getDeepSearch(chessBoard.pieces.size, points.get(ChessSide.WHITE)!!, points.get(ChessSide.BLACK)!!)
-        val move = ai.findNextMove(deepSearch, bitBoardPair.WN, bitBoardPair.BN, false, points.get(ChessSide.WHITE)!!, points.get(ChessSide.BLACK)!!, ChessConfig.WIN_POINT)
-        val endTime = System.currentTimeMillis()
+            val array = TranformBitboardAndView().viewModelToArray(chessBoardRotated)
+            for (i in 0..7) {
+                println(Arrays.toString(array[i]))
+            }
 
-        val moveResult = ai.makeMove(move, true, bitBoardPair.WN, bitBoardPair.BN, points.get(ChessSide.WHITE)!!,
-                points
-                        .get(ChessSide.BLACK)!!)
-        println(Rating.evaluate(moveResult.WN, 0L, 0, 0))
+            val ai = AI(Zobrist())
+            val startTime = System.currentTimeMillis()
+            val deepSearch = getDeepSearch(chessBoard.pieces.size, points.get(ChessSide.WHITE)!!, points.get(ChessSide.BLACK)!!)
+            val move = ai.findNextMove(deepSearch, bitBoardPair.WN, bitBoardPair.BN, false, points.get(ChessSide.WHITE)!!, points.get(ChessSide.BLACK)!!, ChessConfig.WIN_POINT)
+            val endTime = System.currentTimeMillis()
+
+            val moveResult = ai.makeMove(move, true, bitBoardPair.WN, bitBoardPair.BN, points.get(ChessSide.WHITE)!!,
+                    points
+                            .get(ChessSide.BLACK)!!)
+            println(Rating.evaluate(moveResult.WN, 0L, 0, 0))
 
 //        var piece = chessBoardRotated.pieces.filter { it.chessSide == ChessSide.BLACK }.first()
-        printMoves(move)
-        val piece = chessBoard.pieces.filter {
-            //            it.position.x == move[0].toString().toInt() && it.position.y == move[1].toString().toInt()
-            it.position.x == move[1].toString().toInt() && it.position.y == 7 - move[0].toString().toInt()
-        }.first()
+            printMoves(move)
+            val piece = chessBoard.pieces.filter {
+                //            it.position.x == move[0].toString().toInt() && it.position.y == move[1].toString().toInt()
+                it.position.x == move[1].toString().toInt() && it.position.y == 7 - move[0].toString().toInt()
+            }.first()
 
 //        val position = ChessPiece.Position(move[2].toString().toInt(), move[3].toString().toInt())
-        val position = ChessPiece.Position(move[3].toString().toInt(), 7 - move[2].toString().toInt())
-        callback(piece, position)
-        println("piece moved")
+            val position = ChessPiece.Position(move[3].toString().toInt(), 7 - move[2].toString().toInt())
+            callback(piece, position)
+            println("piece moved")
+        }.start()
+
+
     }
 
     fun getDeepSearch(pieceSize: Int, pointOfWhite: Int, pointOfBlack: Int): Int {
-        if (pieceSize > 10) return 5
+        if (pieceSize > 10) return 6
         if (pieceSize > 8) return 6
         if (pieceSize > 6) return 7
         if (pieceSize > 5) return 8
