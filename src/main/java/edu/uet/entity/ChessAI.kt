65b2df567208aa5dd0c36d0b389c56b8b177383d
Tran Minh Tuan <tuantmtb@gmail.com>
2018-01-06 13:20:10 +0700

    rotate 90

--- src/main/java/edu/uet/entity/ChessAI.kt
+++ src/main/java/edu/uet/entity/ChessAI.kt
@@ -21,22 +21,22 @@ class ChessAI(val color: ChessSide = ChessSide.BLACK) {
 //        val currentGameSpace = GameSpace(gameSpaceRoot)
         println("getNextMoveForChessBoard")
         val pieces = chessBoard.pieces
-        for (piece in pieces) {
-            val side = if (piece.chessSide == ChessSide.BLACK) "n" else "N"
-            print("[" + piece.position.x + "][" + piece.position.y + "]=" + side)
-        }
-        println()
-//        val chessBoardRotated = rotateChessBoard90Left(chessBoard)
-        val bitBoardPair = TranformBitboardAndView().viewModelToAIModel(chessBoard)
+//        for (piece in pieces) {
+//            val side = if (piece.chessSide == ChessSide.BLACK) "n" else "N"
+//            print("[" + piece.position.x + "][" + piece.position.y + "]=" + side)
+//        }
+//        println()
+        val chessBoardRotated = rotateChessBoard90Left(chessBoard)
+        val bitBoardPair = TranformBitboardAndView().viewModelToAIModel(chessBoardRotated)
 
-        val array = TranformBitboardAndView().viewModelToArray(chessBoard)
+        val array = TranformBitboardAndView().viewModelToArray(chessBoardRotated)
         for (i in 0..7) {
             println(Arrays.toString(array[i]))
         }
 
         val ai = AI(Zobrist())
         val startTime = System.currentTimeMillis()
-        val move = ai.findNextMove(5, bitBoardPair.WN, bitBoardPair.BN, false, 0, 0)
+        val move = ai.findNextMove(4, bitBoardPair.WN, bitBoardPair.BN, false, 0, 0)
         val endTime = System.currentTimeMillis()
 
         val moveResult = ai.makeMove(move, true, bitBoardPair.WN, bitBoardPair.BN, 0, 0)
@@ -45,12 +45,12 @@ class ChessAI(val color: ChessSide = ChessSide.BLACK) {
 //        var piece = chessBoardRotated.pieces.filter { it.chessSide == ChessSide.BLACK }.first()
         printMoves(move)
         val piece = chessBoard.pieces.filter {
-            it.position.x == move[0].toString().toInt() && it.position.y == move[1].toString().toInt()
-//            it.position.x == move[1].toString().toInt() && it.position.y == 7 - move[0].toString().toInt()
+//            it.position.x == move[0].toString().toInt() && it.position.y == move[1].toString().toInt()
+            it.position.x == move[1].toString().toInt() && it.position.y == 7 - move[0].toString().toInt()
         }.first()
 
-        val position = ChessPiece.Position(move[2].toString().toInt(), move[3].toString().toInt())
-//        val position = ChessPiece.Position(move[3].toString().toInt(), 7 - move[2].toString().toInt())
+//        val position = ChessPiece.Position(move[2].toString().toInt(), move[3].toString().toInt())
+        val position = ChessPiece.Position(move[3].toString().toInt(), 7 - move[2].toString().toInt())
         callback(piece, position)
         println("piece moved")
     }
